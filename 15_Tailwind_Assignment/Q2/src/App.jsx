deabc1c02addd3908c907d16778851c0ad4aed39
viperdotdev <sohaildotcss@gmail.com>
2023-10-30 20:11:42 +0530

    15-Q2/lg  screen responsive done

--- 15_Tailwind_Assignment/Q2/src/App.jsx
+++ 15_Tailwind_Assignment/Q2/src/App.jsx
@@ -3,22 +3,22 @@ import heroBg from "./assets/hero-image.jpg";
 function App() {
   return (
     <>
-      <div className="wrapper lg:w-screen xl:w-[80vw] h-screen ">
+      <div className="wrapper lg:px-3 lg:w-screen xl:w-[80vw] h-screen ">
         <div className="gallery">
           <h1 className="font-semibold text-xl my-4">Image Gallery</h1>
-          <div className="w-full lg:h-[90vh] xl:h-[480px]  flex gap-2">
+          <div className="w-full lg:h-[90vh]  xl:h-[480px]  flex gap-2">
             <img
-              className="w-1/3 object-cover object-top"
+              className="w-1/3  xl:h-auto object-cover object-top"
               src={heroBg}
               alt=""
             />
             <img
-              className="w-1/3 object-cover object-top"
+              className="w-1/3 object-cover object-top xl:h-auto"
               src={heroBg}
               alt=""
             />
             <img
-              className="w-1/3 object-cover object-top"
+              className="w-1/3 object-cover object-top xl:h-auto"
               src={heroBg}
               alt=""
             />
