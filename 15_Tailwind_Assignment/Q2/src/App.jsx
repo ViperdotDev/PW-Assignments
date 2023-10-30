37760dc6cd56b032678f16dc8b9ba53559d2850e
viperdotdev <sohaildotcss@gmail.com>
2023-10-30 20:47:55 +0530

    15-Q2/sm, xl, lg screens responsiveness done

--- 15_Tailwind_Assignment/Q2/src/App.jsx
+++ 15_Tailwind_Assignment/Q2/src/App.jsx
@@ -4,25 +4,23 @@ function App() {
   return (
     <>
       <div className="wrapper lg:px-3 lg:w-screen xl:w-[80vw] h-screen ">
-        <div className="gallery">
-          <h1 className="font-semibold text-xl my-4">Image Gallery</h1>
-          <div className="w-full lg:h-[90vh]  xl:h-[480px]  flex gap-2">
-            <img
-              className="w-1/3  xl:h-auto object-cover object-top"
-              src={heroBg}
-              alt=""
-            />
-            <img
-              className="w-1/3 object-cover object-top xl:h-auto"
-              src={heroBg}
-              alt=""
-            />
-            <img
-              className="w-1/3 object-cover object-top xl:h-auto"
-              src={heroBg}
-              alt=""
-            />
-          </div>
+        <h1 className="font-semibold text-xl my-4">Image Gallery</h1>
+        <div className="w-full sm:h-screen  md:flex-row lg:flex-row xl:flex lg:h-[90vh]  xl:h-[480px] flex-col sm:flex  gap-4 ">
+          <img
+            className="w-1/3 sm:w-full sm:h-[580px] xl:h-auto object-cover object-top"
+            src={heroBg}
+            alt=""
+          />
+          <img
+            className="w-1/3 sm:w-full sm:h-[580px] object-cover object-top xl:h-auto"
+            src={heroBg}
+            alt=""
+          />
+          <img
+            className="w-1/3 sm:w-full sm:h-[700px] object-cover object-top xl:h-auto"
+            src={heroBg}
+            alt=""
+          />
         </div>
       </div>
     </>
