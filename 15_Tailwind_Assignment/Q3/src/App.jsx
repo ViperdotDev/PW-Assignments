ee560e59450a540356e148b568dda298e03680ed
viperdotdev <sohaildotcss@gmail.com>
2023-10-31 06:33:54 +0530

    Vite with Tailwind + react-icons setup

---
+++ 15_Tailwind_Assignment/Q3/src/App.jsx
@@ -0,0 +1,13 @@
+import { FaEnvelope } from "react-icons/fa6";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-red-300">Helloo</h1>
+      <FaEnvelope/>
+    </>
+  );
+}
+
+export default App;
